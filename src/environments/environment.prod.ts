3a9951971ed839974bb79e18a75898b0befe3f9f
Parag Saxena <parag.saxena@confluxsys.com>
2021-06-09 00:55:49 +0530

    json api path updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://my-json-server.typicode.com/paragsaxena8/agriGrowth/',
+  api: 'https://my-json-server.typicode.com/rahul-2512/agriGrowth/',
   cowin_api: 'https://cdn-api.co-vin.in/api/v2'
 };

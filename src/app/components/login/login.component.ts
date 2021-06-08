c9204122159f2707ffa13362a88ebd8ee76c4945
Parag Saxena <parag.saxena@confluxsys.com>
2021-06-09 01:28:59 +0530

    Login updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,41 +1,50 @@
 import { ToastrService } from 'ngx-toastr';
 import { HttpService } from './../../services/http.service';
 import { GlobalService } from './../../services/global-service.service';
-import { Component,  OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  active = 2;
   userLoginForm = {
     email: '',
-    password: ''
-  }
-  adminLoginForm = {
-    email: '',
-    password: ''
-  }
-  constructor(private gs:GlobalService, private ht:HttpService, private toastr:ToastrService) { }
+    password: '',
+  };
+  constructor(
+    private gs: GlobalService,
+    private ht: HttpService,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  loginIn(){    
-    this.ht.getUser().subscribe((users:any)=> {
-      for (let index = 0; index < users.length; index++) {
-        if(users[index] && users[index].email === this.userLoginForm.email && users[index].password === this.userLoginForm.password){            
-          this.gs.isAuthenticated(users[index]);
-          return;
-        } 
-        this.toastr.error('Email or Password Might be incorrect')        
+  loginIn() {
+    this.ht.getUser().subscribe(
+      (users: any) => {
+        for (let index = 0; index < users.length; index++) {
+          if (
+            users[index] &&
+            users[index].email === this.userLoginForm.email &&
+            users[index].password === this.userLoginForm.password
+          ) {
+            this.gs.isAuthenticated(users[index]);
+            return;
+          }
+          if (
+            !users[index] &&
+            users[index].email !== this.userLoginForm.email &&
+            users[index].password !== this.userLoginForm.password
+          ) {
+            this.toastr.error('Email or Password Might be incorrect');
+          }
+        }
+      },
+      (error) => {
+        this.toastr.error('Something Went Wrong, Try Again Later');
       }
-    }, (error)=> {
-      this.toastr.error('Something Went Wrong, Try Again Later');
-    })
+    );
   }
-
-
 }
